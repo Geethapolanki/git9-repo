a12ee4d7ca0a57ded1a41c18bded820e2cabbd1f
Geethapolanki <104352460+Geethapolanki@users.noreply.github.com>
2022-05-08 09:28:45 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from unicodedata import name
+from flask import Flask
+app=Flask(__name__)
+
+@app. route('/')
+def greet():
+    return "hello geetha"
+
+if __name__== "__main__":
+    app.run(debug=True,host="0.0.0.0")
